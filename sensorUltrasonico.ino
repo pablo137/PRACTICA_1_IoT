f495bc75a9310d9e424e518527236b9b0a1b1ac0
pablo137 <p48102611@gmail.com>
2024-03-11 12:35:46 -0400

    Implementando clases

--- sensorUltrasonico.ino
+++ sensorUltrasonico.ino
@@ -1,66 +1,91 @@
-#define trigger 15
-#define echo 16
-float cm = 0;
+class UltrasonicSensor {
+private:
+  int triggerPin;
+  int echoPin;
 
-int ports[3] = { 32, 33, 26 };  //Puertos del MCU
+public:
+  UltrasonicSensor(int triggerPin, int echoPin) {
+    this->triggerPin = triggerPin;
+    this->echoPin = echoPin;
+  }
+
+  void setup() {
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    digitalWrite(triggerPin, LOW);
+  }
+
+  float readDistance() {
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+    unsigned long duration = pulseIn(echoPin, HIGH);
+    float distance = duration * 0.0343 / 2;  // Calcula la distancia en centímetros
+    return distance;
+  }
+};
+
+class LEDController {
+private:
+  int ledPins[3];
+
+public:
+  LEDController(int pin1, int pin2, int pin3) {
+    ledPins[0] = pin1;
+    ledPins[1] = pin2;
+    ledPins[2] = pin3;
+  }
+
+  void setup() {
+    for (int i = 0; i < 3; i++) {
+      pinMode(ledPins[i], OUTPUT);
+    }
+  }
+
+  void setLED(int index, bool state) {
+    digitalWrite(ledPins[index], state ? HIGH : LOW);
+  }
+};
+
+const int triggerPin = 15;
+const int echoPin = 16;
+
+const int ledPins[3] = {32, 33, 23};  // Pines de los LEDs
+
+UltrasonicSensor ultrasonicSensor(triggerPin, echoPin);
+LEDController ledController(ledPins[0], ledPins[1], ledPins[2]);
 
 void setup() {
-  for (int i = 0; i < 3; i++) {
-    pinMode(ports[i], OUTPUT);
-  }
-  pinMode(trigger, OUTPUT);
-  pinMode(echo, INPUT);
-  digitalWrite(trigger, LOW);  //Inicializamos el pin con 0
   Serial.begin(115200);
-}
-
-long readUltrasonicDistance(int triggerPin, int echoPin) {
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  return pulseIn(echoPin, HIGH);  //Se devolvera el tiempo entre el envio y la recepcion
+  ultrasonicSensor.setup();
+  ledController.setup();
 }
 
 void measureDistance(float cm) {
+  ledController.setLED(0, false);
+  ledController.setLED(1, false);
+  ledController.setLED(2, false);
+
   if (cm >= 0 && cm < 50) {
-    digitalWrite(ports[1], LOW);
-    digitalWrite(ports[2], LOW);
-    digitalWrite(ports[0], HIGH);
+    ledController.setLED(0, true);
     delay(1000);
-    digitalWrite(ports[0], LOW);
+    ledController.setLED(0, false);
+  } else if (cm >= 50 && cm <= 150) {
+    ledController.setLED(1, true);
     delay(1000);
-  } else if (cm > 50 & cm <= 150) {
-
-    digitalWrite(ports[0], LOW);
-    digitalWrite(ports[2], LOW);
-    digitalWrite(ports[1], HIGH);
-    delay(1000);
-    digitalWrite(ports[1], LOW);
-    delay(1000);
-  }
-
-  else if (cm > 150 && cm <= 300) {
-    digitalWrite(ports[0], LOW);
-    digitalWrite(ports[2], LOW);
-    digitalWrite(ports[1], HIGH);
-    delay(1000);
-  }
-
-  else if (cm > 300) {
-    digitalWrite(ports[0], LOW);
-    digitalWrite(ports[1], LOW);
-    digitalWrite(ports[2], HIGH);
-    delay(1000);
+    ledController.setLED(1, false);
+  } else if (cm > 150 && cm <= 300) {
+    ledController.setLED(1, true);
+  } else if (cm > 300) {
+    ledController.setLED(2, true);
   }
 }
 
 void loop() {
-  cm = 0.01723 * readUltrasonicDistance(trigger, echo);  //se calculara la distancia multiplicando la velocidad en la que el sonido recorre un centimetro por el tiempo de rebote obtenido.
+  float distance = ultrasonicSensor.readDistance();
   Serial.print("Distancia: ");
-  measureDistance(cm);
-  Serial.print(cm);
+  Serial.print(distance);
   Serial.println("cm");
+  measureDistance(distance);
+  delay(1000);
 }
